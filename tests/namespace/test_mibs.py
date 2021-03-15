a8c6e360eb572ff79bde088ed09057153c5bc440
Qi Luo <qiluo-msft@users.noreply.github.com>
2021-03-15 14:28:55 -0700

    Implement rfc4363 FdbUpdater for lag inside vlan (#204)
    - What I did
    Same as #203
    But target on 201911 branch.

--- tests/namespace/test_mibs.py
+++ tests/namespace/test_mibs.py
@@ -21,7 +21,7 @@ class TestGetNextPDU(TestCase):
 
         lag_name_if_name_map, \
         if_name_lag_name_map, \
-        oid_lag_name_map = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, dbs)
+        oid_lag_name_map, _ = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, dbs)
         #PortChannel in asic0 Namespace
         self.assertTrue(b"PortChannel01" in lag_name_if_name_map)
         self.assertTrue(b"Ethernet-BP0" in lag_name_if_name_map[b"PortChannel01"])
--- tests/test_mibs.py
+++ tests/test_mibs.py
@@ -21,7 +21,7 @@ class TestGetNextPDU(TestCase):
 
         lag_name_if_name_map, \
         if_name_lag_name_map, \
-        oid_lag_name_map = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, db_conn)
+        oid_lag_name_map, _ = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, db_conn)
 
         self.assertTrue(b"PortChannel04" in lag_name_if_name_map)
         self.assertTrue(lag_name_if_name_map[b"PortChannel04"] == [b"Ethernet124"])
